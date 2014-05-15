1f3968eb3593204d6b7100b9264f2cc7356f40ec
Alexander Simeonov <simeonov@gmail.com>
2014-05-15 10:12:03 +0300

    updated theme settings, readme.txt

--- audiolize_article.php
+++ audiolize_article.php
@@ -1,81 +1,92 @@
 <?php
 /**
- * Plugin Name: Audiolize Article
- * Plugin URI: http://www.audiolize.net
- * Description: Automatically injects audiolize functionality in wordpress articles.
- * Version: 0.1
- * Author: audiolize
- * Author URI: http://www.audiolize.net
+ * Plugin Name:     audiolize.net
+ * Plugin URI:      https://github.com/audiolize/audiolize-wordpress
+ * Description:     Automatically injects audiolize.net functionality in wordpress articles.
+ * Version:         1.0
+ * Author:          audiolize GmbH
+ * Author URI:      http://www.audiolize.net
  */
 
 if( !defined( 'ABSPATH' ) )
 	exit;
 
 /* 
- * adds audiolize_widget function to tamplates scope
+ * adds audiolize_widget function to templates scope
  */
 
-function audiolize_widget(){
+function audiolize_widget() {
 	do_action('audiolize_widget');
 }
 
 class AudiolizeWidget {
-	private $theme_choices = array(
-		'full' => 'full',
-		'wide' => 'wide',
-		'minimal' => 'minimal',
-	);
+	private $themes = array(
+            "minimal" => "Minimal theme",
+            "wide" => "Wide theme",
+            "full" => "Full theme",
+            "tiny" => "Tiny theme"
+    );
+    
+    private $widget_host = 'https://dev.audiolize.net';
 	
-	function settings_menu(){
+	function settings_menu() {
 		require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-		add_submenu_page('options-general.php', 'Audiolize widget', 'Audiolize widget', 'manage_options', 'audiolize_widget_options', array($this, 'settings_page' ));
-	} 
+		add_submenu_page(
+		    'options-general.php',
+		    'audiolize.net',
+		    'audiolize.net',
+		    'manage_options',
+		    'audiolize_widget_options',
+		    array($this, 'settings_page' )
+        );
+	}
 	
-	function __construct(){
-		add_option('audiolize_widget_theme', 'full', '', 'yes');
+	function __construct() {
+		add_option('audiolize_widget_theme', 'tiny', '', 'yes');
 		add_action('audiolize_widget', array( $this, 'inject_audiolize_widget'));
 		add_action('admin_menu', array( $this, 'settings_menu' ) );
 	}
 	
-	function inject_audiolize_widget(){
-		$theme = get_option('audiolize_widget_theme', 'full');
+	function inject_audiolize_widget() {
+		$theme = get_option('audiolize_widget_theme', 'tiny');
 		$article_url = get_permalink();
 		$widget_html = <<<HTML
-		<script id="audiolize-widgets-loader" type="text/javascript" src="https://dev.audiolize.net/static/js/widgets/loader.js" data-audiolize-theme="$theme" data-audiolize-domain="https://dev.audiolize.net"></script>
-	<div data-audiolize-article_canonical_url="$article_url" data-audiolize-article_version="1.0"></div>
+    		<script id="audiolize-widgets-loader" type="text/javascript" src="$widget_host/static/js/widgets/loader.js" data-audiolize-theme="$theme" data-audiolize-domain="$widget_host"></script>
+	        <div data-audiolize-article_canonical_url="$article_url" data-audiolize-article_version="1.0"></div>
 HTML;
 		echo $widget_html;
 	}
 
-	function settings_page(){
+	function settings_page() {
 		if ( isset( $_POST['submit'] ) ) {
 			update_option('audiolize_widget_theme', $_POST['audiolize_widget_theme']);
 		}
-		$theme = get_option('audiolize_widget_theme', 'full');
+		
+        $options = "";
+
+        foreach ($themes as $key => $value) {
+    		$selected = ($key == get_option('audiolize_widget_theme', 'tiny') ? ' selected="selected"' : '');
+    		$options .= "<option value='$key' $selected>$value</option>";
+        }
+		
 		$html = <<<HTML
 		<form method="post" action="" id="audiolize_widget_options_form">
 			<table>
-					<tr>
-						<td>
-							<label for="audiolize_widget_theme">Audiolize widget theme (minimal, wide, full)</label>			
-						</td>
-						<td>
-							<input name="audiolize_widget_theme" type="text" id="audiolize_widget_theme" value="$theme" class="text">
-							<!--
-							<select name="audiolize_widget_theme" id="audiolize_widget_theme">
-								<option value='minimal'>minimal</option>
-								<option value='wide'>wide</option>
-								<option value='full'>full</option>
-							</select>
-							-->
-						</td>
-					</tr>
+				<tr>
+					<td><label for="audiolize_widget_theme">Audiolize widget theme</label>:</td>
+					<td><select name="audiolize_widget_theme" id="audiolize_widget_theme">$options</select></td>
+				</tr>
 			</table>
-			<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>
+			<p class="submit">
+			    <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+			</p>
 		</form>
 HTML;
 		echo $html;
 	}
 }
+
 new AudiolizeWidget();
+
 ?>
+
--- readme.txt
+++ readme.txt
@@ -1,13 +1,15 @@
-﻿Audiolize Widget for WordPress.
+=== audiolize.net Widget for WordPress ===
 
-Automatically injects Audiolize functionality in WordPress articles.
+Automatically injects audiolize.net functionality in WordPress articles.
 
-USAGE:
+== Usage ==
 
-1.) Install the plugin using WordPress administrative panel.
-2.) You can add <?php audiolize_widget();?> within any WordPress theme you want to use the plugin.
+1. Install the plugin using WordPress administrative panel.
+1. Go to Settings -> audiolize.net section to configure the widget's theme that you want to use.
+1. Add `<?php audiolize_widget();?>` within the WordPress theme's content.php 
+1. Sign in your audiolize.net content provider account and open any article in your WordPress website. The audiolize.net widget should be visible on the page.
 
-Example: 
-	Edit content.php of your default theme and add <?php audiolize_widget();?> immediately before <div class="entry-content">.
+**Example:**
 
-3) Sign in your audiolize.net content provider account and load any article from you WordPress page. The Audiolize widget should be visible.
+In content.php of your default theme and add `<?php audiolize_widget();?>` immediately before `<div class="entry-content">`.
+
